d3ed748cfff47a026e32c315563b07e091666274
2sandeep7 <ssandeep8696@gmail.com>
2024-01-20 18:24:30 -0500

    Done with Setting up till Building Login Page

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { GoogleLogin } from '@react-oauth/google'
+import { useNavigate } from 'react-router-dom'
+import { FcGoogle } from "react-icons/fc";
+const Login = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Login
