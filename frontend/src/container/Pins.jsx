126fc68f7c6db48c31f898165069cfb4a66b7763
2sandeep7 <ssandeep8696@gmail.com>
2024-01-24 22:42:16 -0500

    Implementation of Navbar and Other compo

--- frontend/src/container/Pins.jsx
+++ frontend/src/container/Pins.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
-const Pins = () => {
+import React,{ useState} from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Navbar,Feed,PinDetails,CreatePin,Search} from '../components/index'
+const Pins = ({user}) => {
+  const [searchTerm,setSearchTerm]=useState('')
   return (
-    <div>
-       Pins
+    <div className='px-2 md:px-5'> 
+       <div
+       className='bg-gray-50'>
+       <Navbar
+       user={user}
+       searchTerm={searchTerm}
+       setSearchTerm={setSearchTerm}
+       />
+       </div>
+       <div className='h-full'>
+    <Routes>
+      <Route path='/' element={<Feed />} />
+      <Route path='/category/:categoryId' element={<Feed />} />
+      <Route path='/pin-detail/:pinId' element={<PinDetails user={user} />} />
+      <Route path='/create-pin' element={<CreatePin user={user} />} />
+      <Route path='/search' element={<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />} />
+    </Routes>
+       </div>
     </div>
   )
 }
